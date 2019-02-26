a831964304c5fbb5daf4dc271816dfd372f959e2
Alain Hélaïli <helaili@github.com>
2019-02-26 21:04:25 +0100

    update status

---
+++ actions/DeployStatusUpdateAction/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -e
+
+DEPLOYMENT_STATUS_URL=$(jq -r .deployment.statuses_url $GITHUB_EVENT_PATH)
+DEPLOYMENT_ENVIRONMENT=$(jq -r .deployment.environment $GITHUB_EVENT_PATH)
+
+DESCRIPTION="Deployed in $DEPLOYMENT_ENVIRONMENT"
+TARGET_URL=$*
+
+
+echo $GITHUB_TOKEN
+echo $DEPLOYMENT_ENVIRONMENT
+echo $DEPLOYMENT_STATUS_URL
+echo $TARGET_URL
+
+JSON_STRING=$( jq -n \
+                  --arg desc "$DESCRIPTION" \
+                  --arg url "$TARGET_URL" \
+                  '{"state": "success", "description": $desc, "environment_url": $url}' )
+
+curl -k -H "Authorization: token $GITHUB_TOKEN" -d "$JSON_STRING" $DEPLOYMENT_STATUS_URL
